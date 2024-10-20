8f7a2c3fe41184d5cdf4e25caea9958452a1f3f6
Ryan Ng <ropherng@Ryans-MacBook-Pro.local>
2024-10-19 19:53:43 -0700

    asd

--- probana-frontend/app/components/market.js
+++ probana-frontend/app/components/market.js
@@ -2,13 +2,12 @@ import Link from "next/link";
 import { useState, useEffect } from "react"
 import { IoMdClose } from "react-icons/io";
 
-export default function Market({ marketDetail }) {
+export default function Market({ market, odds }) {
 
-    const key = marketDetail.key
-    const prompt = marketDetail.prompt
-    const option1 = marketDetail.option1
-    const option2 = marketDetail.option2
-    const odds = marketDetail.odds
+    const id = market.marketId
+    const yesLabel = market.yesLabel
+    const noLabel = market.noLabel
+    const name = market.name
 
     return (
         <div className="bg-[#2C3F50] rounded-md p-[20px] hover:bg-[#425465]">
@@ -16,7 +15,7 @@ export default function Market({ marketDetail }) {
             <div className="flex flex-col justify-between gap-[4px] grow h-full">
                 <div className="flex flex-row justify-between gap-[10px]">
                     <h3>
-                        {prompt}
+                        {name}
                     </h3>
                     <h5>
                         {odds * 100}%
@@ -25,14 +24,15 @@ export default function Market({ marketDetail }) {
                 <div className="flex flex-row justify-between gap-[10px]">
                     <Link
                         className="bg-[#2C4B51] py-[8px] px-[8px] rounded-sm hover:bg-[#27ae60] text-[#27ae60] hover:text-white grow"
-                        href={{ pathname: '/marketprompt', query: prompt }}>
-                        {option1}
+                        href={'/market/' + id}
+                    >
+                        {yesLabel}
                     </Link>
                     <Link
                         className="bg-[#414052] py-[8px] px-[8px] rounded-sm text-[#e64800] hover:bg-[#e64800] hover:text-white grow"
-                        href={`/market?prompt=${prompt}`}
+                        href={`/market/` + id}
                     >
-                        {option2}
+                        {noLabel}
                     </Link>
                 </div>
             </div>
