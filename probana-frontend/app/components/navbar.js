2adf1619a5c924669806986deb8631eef2d08e78
Elliot <112030301+BraverElliot@users.noreply.github.com>
2024-10-19 22:45:36 -0700

    mg

--- probana-frontend/app/components/navbar.js
+++ probana-frontend/app/components/navbar.js
@@ -5,10 +5,60 @@ import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import ApproveSpending from './approveSpending';
 
+import { useState } from 'react';
+import { useDynamicContext } from '@dynamic-labs/sdk-react-core';
+import { isEthereumWallet } from '@dynamic-labs/ethereum';
+import { ethers } from 'ethers';
+
+
+
+
 function Modal({ modalOpen, setModalOpen }) {
   const [prompt, setPrompt] = useState(null)
   const [option1, setOption1] = useState(null)
   const [option2, setOption2] = useState(null)
+
+  const contractAddress = "0x7f27352D5F83Db87a5A3E00f4B07Cc2138D8ee52";
+  const spenderAddress = "0x7A0aE150F6E03f6B038B673c7B32341496F65f41";
+  const amount = ethers.utils.parseUnits("1.0", 18); // Convert 1.0 ETH to wei
+  const { primaryWallet } = useDynamicContext();
+  const [txnHash, setTxnHash] = useState('');
+
+  if (!primaryWallet || !isEthereumWallet(primaryWallet)) return null;
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    // const spender = formData.get('address');
+    // const amountToApprove = formData.get('amount');
+
+    const publicClient = await primaryWallet.getPublicClient();
+    const walletClient = await primaryWallet.getWalletClient();
+
+    // Define the ABI of the contract function you want to call
+    const abi = [
+      "function approve(address spender, uint256 amount) public returns (bool)"
+    ];
+    const iface = new ethers.utils.Interface(abi);
+
+    // Encode the function call
+    const data = iface.encodeFunctionData("approve", [spenderAddress, amount]);
+
+    const transaction = {
+      to: contractAddress,
+      data: data,
+    };
+
+    const hash = await walletClient.sendTransaction(transaction);
+    setTxnHash(hash);
+
+    const receipt = await publicClient.getTransactionReceipt({
+      hash,
+    });
+
+    console.log(receipt);
+  };
 
   return (
     <div className="absolute w-full h-full bg-[rgba(0,0,0,0.5)] items-center justify-center flex" onClick={() => { setModalOpen(false) }}>
