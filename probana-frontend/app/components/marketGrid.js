b0080966eb99e2efd28fa0ad5a3d7ab9f68b9a0a
Ryan Ng <ropherng@Ryans-MacBook-Pro.local>
2024-10-19 19:26:05 -0700

    new subgraph link

--- probana-frontend/app/components/marketGrid.js
+++ probana-frontend/app/components/marketGrid.js
@@ -1,8 +1,25 @@
 import Market from "./market";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function MarketGrid({marketDetails}) {
 
+    const [marketList, setMarketList] = useState(null);
+
+    useEffect(() => {
+
+        async function fetchMarketData() {
+            const response = await axios.post("https://subgraph.satsuma-prod.com/97d738dd3352/elliots-team--201737/Probana/version/v0.0.1-new-version/api", `{"query":"{ marketCreateds(first: 500) { name id rules yesLabel noLabel creator blockTimestamp marketId } marketCloseds(first: 500) { id blockTimestamp marketId } }"}`)
+
+            console.log(response.data)
+            // setMarketList(found)
+
+        }
+
+
+        fetchMarketData();
+
+    }, []);
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-[20px] px-[100px] pt-[50px]">
